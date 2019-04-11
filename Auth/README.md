aaece84b9009358d33f4be50da81c133619114d1
Nicolas Grimault <nicolas.g21@live.fr>
2019-04-11 16:33:11 +0200

    Auth module

--- Auth/README.md
+++ Auth/README.md
@@ -1,1 +1,18 @@
 # GitHub-Insights Auth
+
+The main goal of this project is to request the github api to get a token access
+
+This project is realised with  ``Node js``
+
+https://nodejs.org/en/
+
+## Dependencies
+
+this project is using request and express:
+
+```
+NPM install request express
+```
+https://www.npmjs.com/package/request
+
+https://www.npmjs.com/package/express
--- Auth/auth.js
+++ Auth/auth.js
@@ -1,19 +1,27 @@
-/*var ClientOAuth2 = require('client-oauth2')
- 
-var githubAuth = new ClientOAuth2({
-  clientId: '27b974de82fe9b4430bc',
-  redirectUri: 'http://localhost:8080/oauth/redirect',
-  scopes: ['repo','read:user']
-})*/
-	
-// Import the express lirbary
+const request = require('request');
 const express = require('express')
 
-// Create a new express application and use
-// the express static middleware, to serve all files
-// inside the public directory
+const clientId = 'df26091268bce90e6f1e'
+const clientSecret = '54a9e22f48fed4ebf0c253499e0ea5713fa52b1e'
+
 const app = express()
+
+app.get('/callback', function (req, response) {
+  var options = {
+    url: `https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${req.query.code}`,
+    method: 'GET',
+    headers: { 'Accept': 'application/json' },
+  }
+  request(options, function (error, gitResponse, body) {
+    if (!error && gitResponse.statusCode == 200) {
+      response.send(JSON.parse(body).access_token)
+    }
+  })
+})
+
 app.use(express.static(__dirname + '/public'))
 
-// Start the server on port 8080
 app.listen(8080)
+
+
+
